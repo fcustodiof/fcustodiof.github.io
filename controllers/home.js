f75ebf6f95597b445a76a8a0e84534ed84a98cda
Felipe <felipe.custodio.f@gmail.com>
2018-01-27 21:13:55 -0200

    header pronto

--- controllers/home.js
+++ controllers/home.js
@@ -4,9 +4,17 @@ angular.module('home',[])
     $(document).ready(function(){
         $('.parallax').parallax();
         $('.modal').modal();
+        $(".button-collapse").sideNav();
+        $('.dropdown-button').dropdown();
     });
     $scope.inscrever = function(){
         $location.path('/inscrever');
     };
-    
+    var $doc = $('html, body');
+    $('.scrollSuave').click(function() {
+        $doc.animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 500);
+        return false;
+    });
 })
